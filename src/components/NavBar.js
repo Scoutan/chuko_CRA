f485e3eb21fea6b9e0615c556b4d443bb5b6e72d
Scoutan <johnson.chungck@gmail.com>
2021-04-23 16:52:00 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,16 +5,16 @@ const NavBar = () => {
   return (
     <div className="nav">
       <div className="nav_link">
-        <a href="">Home</a>
+        <a href="/">Home</a>
       </div>
       <div className="nav_link">
-        <a href="">Item Watches</a>
+        <a href="/">Item Watches</a>
       </div>
       <div className="nav_link">
-        <a href="">FAQ</a>
+        <a href="/">FAQ</a>
       </div>
       <div className="nav_link">
-        <a href="">Contact</a>
+        <a href="/">Contact</a>
       </div>
       <SignIn />
     </div>
